57b2de737b313fa1bf8da1c36a2cd973a06d96e8
dm1tro69 <dimolg22@gmail.com>
2021-07-14 21:39:52 +0300

    add layout

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,13 @@
+import {getAllEvents} from "../../dummy-data";
+import EventList from "../../components/events/Eventlist";
 
 const AllEventsPage = () => {
+
+    const events = getAllEvents()
+
     return (
         <div>
-            <h1>All Events Page</h1>
+           <EventList items={events}/>
         </div>
     );
 };
